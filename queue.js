649116a42baae1ff084e5947fbfd04cdd6d769a6
rusli abdul gani <rusli.gani88@gmail.com>
2017-06-21 15:39:23 +0700

    stack & queue done

--- queue.js
+++ queue.js
@@ -1,7 +1,59 @@
 'use strict'
 
 class Queue {
-  constructor () {
-
+  constructor (maks) {
+    this.antrian = []
+    this.MAX_SIZE = maks
   }
+
+ queueSize(){
+   return this.antrian.length;
+ }
+
+ queueInsert(nilai) {
+   if (this.isFull())
+   return `antrian sudah penuh, max antrian = ${this.MAX_SIZE}, harus di pop dahulu supaya antrian baru bisa masuk`;
+   else
+   return this.antrian.push(nilai);
+ }
+
+ queueRemove() {
+   if (this.queueSize() == 0)
+   return `Antrian sudah habis`;
+   return this.antrian.shift();
+ }
+
+ first() {
+   return this.antrian[0]
+ }
+
+ last() {
+   return this.antrian[this.size()-1]
+ }
+
+ isEmpty() {
+   if (this.queueSize() === 0)
+   return `antrian kosong`
+   else
+   `antrian masih ada isi sebanyak ${this.queueSize()}`
+ }
+
+ isFull() {
+   return (this.queueSize() === this.MAX_SIZE)
+   // else
+   // return `Tumpukan masih bisa diisi, max: ${this.MAX_SIZE} dan saat ini: ${this.size()}`
+ }
+
+ queuePeep() {
+   return console.log(this.first());
+ }
 }
+
+let antriaBCA = new Queue(3);
+
+console.log(antriaBCA.queueInsert('Adi'));
+console.log(antriaBCA.queueInsert('Budi'));
+console.log(antriaBCA.queueInsert('Chandra'));
+console.log(antriaBCA.queueInsert('Dodi'));
+
+console.log(antriaBCA.queueRemove());
--- stack.js
+++ stack.js
@@ -1,33 +1,74 @@
 'use strict'
 
 // Your code implementation
-class Stack {
-  constructor() {
+class Stack{
+  constructor(max) {
+    this.tumpukan = [];
+    this.MAX_SIZE = max;
   }
 
   size() {
+    return this.tumpukan.length;
   }
 
-  stackPush(x) {
+  stackPush(nilai) {
+    if (this.isFull()) return `tumpukan sudah penuh, max tumpukan = ${this.MAX_SIZE}`;
+    else return this.tumpukan.push(nilai);
   }
 
   stackPop() {
+    if (this.size() == 0) return `Tumpukan sudah habis`;
+    return this.tumpukan.pop();
   }
 
   first() {
+    return this.tumpukan[0]
   }
 
   last() {
+    return this.tumpukan[this.size()-1]
   }
 
   isEmpty() {
+    if (this.size() === 0)
+      return `tumpukan kosong`
+    else
+      `tumpukan masih ada isi yaitu, ${this.size()}`
   }
 
   isFull() {
+    return (this.size() === this.MAX_SIZE)
+
   }
 
   stackPeep() {
+    return console.log(this.first());
   }
 }
 
+let tumpukanBaru = new Stack(10);
+
+tumpukanBaru.stackPush(1);
+tumpukanBaru.stackPush(2);
+tumpukanBaru.stackPush(3);
+tumpukanBaru.stackPush(4);
+tumpukanBaru.stackPush(5);
+tumpukanBaru.stackPush(6);
+tumpukanBaru.stackPush(7);
+tumpukanBaru.stackPush(8);
+tumpukanBaru.stackPush(9);
+tumpukanBaru.stackPush(10);
+tumpukanBaru.stackPush(11);
+tumpukanBaru.first();
+tumpukanBaru.last();
+
+// tumpukanBaru.stackPop();
+console.log(tumpukanBaru.stackPop());
+console.log(tumpukanBaru.stackPop());
+console.log(tumpukanBaru.stackPop());
+console.log(tumpukanBaru.stackPop());
+console.log(tumpukanBaru.first());
+console.log(tumpukanBaru.last());
+console.log(tumpukanBaru.size());
+//console.log(tumpukanBaru.stackPush('ulala'));
 module.exports = Stack
