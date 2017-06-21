113d8a56189267769b66748f01bea53af73d637f
Oscar Hermawan <email": null>
2017-06-21 20:11:08 +0700

    stack and queue

--- queue.js
+++ queue.js
@@ -1,7 +1,61 @@
 'use strict'
 
 class Queue {
-  constructor () {
+  constructor() {
+    this.test = [];
+  }
 
+  size() {
+    return this.test.length;
+  }
+
+  queuePush(x) {
+    return this.test.push(x);
+  }
+
+  queuePop() {
+    return this.test.shift()
+  }
+
+  first() {
+    return this.test[0]
+  }
+
+  last() {
+    return this.test.slice(-1)[0]
+  }
+
+  isEmpty() {
+    if (this.test.length !== 0) {
+      return false;
+    } else {
+      return true
+    }
+  }
+
+  isFull() {
+    if (this.test.length === 0) {
+      return false;
+    } else {
+      return true
+    }
+  }
+
+  queuePeep() {
+    return this.test[0]
   }
 }
+
+let mystack = new Queue()
+
+console.log(`The stack is empty ? ${mystack.isEmpty()}`);
+console.log(`The stack is full ? ${mystack.isFull()}`);
+mystack.queuePush('Javascript');
+mystack.queuePush('is just so');
+mystack.queuePush('cool');
+mystack.queuePush(12);
+mystack.queuePop()
+console.log(mystack.last());
+
+console.log(mystack.queuePeep());
+console.log(mystack.size());
--- stack.js
+++ stack.js
@@ -3,31 +3,63 @@
 // Your code implementation
 class Stack {
   constructor() {
+    this.test = [];
   }
 
   size() {
+    return this.test.length;
   }
 
   stackPush(x) {
+    return this.test.push(x);
   }
 
   stackPop() {
+    return this.test.pop()
   }
 
   first() {
+    return this.test[0]
   }
 
   last() {
+    return this.test.slice(-1)[0]
   }
 
   isEmpty() {
+    if (this.test.length !== 0) {
+      return false;
+    } else {
+      return true
+    }
   }
 
   isFull() {
+    if (this.test.length === 0) {
+      return false;
+    } else {
+      return true
+    }
   }
 
   stackPeep() {
+    return this.test.slice(-1).pop()
   }
 }
+
+let mystack = new Stack()
+
+console.log(`The stack is empty ? ${mystack.isEmpty()}`);
+console.log(`The stack is full ? ${mystack.isFull()}`);
+mystack.stackPush('Javascript');
+mystack.stackPush('is just so');
+mystack.stackPush('cool');
+mystack.stackPush(12);
+mystack.stackPop()
+console.log(mystack.last());
+
+console.log(mystack.stackPeep());
+console.log(mystack.size());
+
 
 module.exports = Stack
