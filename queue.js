f74495ef20a041d8e98d1678ee32f78db70e06c0
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-06-21 16:05:40 +0700

    stack and queue done

--- queue.js
+++ queue.js
@@ -2,6 +2,32 @@
 
 class Queue {
   constructor () {
+      this.array = [];
+  }
 
+  queuePush(value){
+    this.array.push(value);
+  }
+
+  queuePop(){
+    this.array.shift();
+  }
+
+  listArray(){
+    return this.array;
   }
 }
+
+
+let myQueue = new Queue();
+
+console.log(myQueue);
+
+myQueue.queuePush('anto');
+myQueue.queuePush('andini');
+myQueue.queuePush('yanto');
+myQueue.queuePush('tomat');
+
+console.log(myQueue.listArray());
+myQueue.queuePop();
+console.log(myQueue.listArray());
--- stack.js
+++ stack.js
@@ -3,31 +3,68 @@
 // Your code implementation
 class Stack {
   constructor() {
+    this.array = [];
   }
 
   size() {
+    return this.array.length;
   }
 
   stackPush(x) {
+    this.array.push(x);
   }
 
   stackPop() {
+    this.array.pop();
+    return 'data paling belakang di hapus'
   }
 
   first() {
+    return this.array[0];
   }
 
   last() {
+    return this.array[this.size()-1];
   }
 
   isEmpty() {
+    if(this.size() == 0) return true;
+    else return false;
   }
 
   isFull() {
+
   }
 
   stackPeep() {
+    console.log(this.array[this.size()-1]);
   }
+
+  allArray(){
+    return this.array;
+  }
+
 }
 
-module.exports = Stack
+let myStack = new Stack();
+
+console.log(`the stack is empty ${myStack.isEmpty()}`);
+
+myStack.stackPush('javascript');
+myStack.stackPush('is just so');
+myStack.stackPush('cool');
+
+myStack.stackPush(15);
+
+myStack.stackPeep();
+console.log(myStack.allArray());
+
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+
+console.log(`the stack is empty? ${myStack.isEmpty()}`);
+
+console.log(myStack.allArray());
+module.exports = Stack;
