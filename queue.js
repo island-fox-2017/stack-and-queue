170f5685e5c508dc5fdea57a022b61f953e9f435
kokoh <kokohtan@gmail.com>
2017-06-21 17:09:00 +0700

    add stacknqueue

--- queue.js
+++ queue.js
@@ -1,7 +1,89 @@
 'use strict'
 
 class Queue {
-  constructor () {
+  constructor (max) {
+    this.arr= [];
+    this.max= max;
+  }
 
+  size() {
+    return this.arr.length;
+  }
+
+  queuePush(x) {
+    this.arr.unshift(x)
+  }
+
+  queuePop() {
+    if (this.isEmpty()) {
+      return "You can get in, its Empty";
+    }
+    else {
+      this.arr.pop();
+      return this.queuePeep();
+    }
+  }
+
+  first() {
+    return this.arr[this.arr.length-1];
+  }
+
+  last() {
+    return this.arr[0];
+  }
+
+  isEmpty() {
+    if (this.size() === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  isFull() {
+    if (this.size() === this.max) {
+      return 'Its Full';
+    }
+    else {
+      return `Come In, there's still plenty of room`;
+    }
+  }
+
+  queuePeep() {
+    return this.arr
   }
 }
+
+//buat queue baru
+let myQueue = new Queue();
+//Check apakah Queue sudah ada isi atau kosong ?
+console.log(`The Queue is empty ? ${myQueue.isEmpty()}`) // true
+
+//Tambahkan tiga
+myQueue.queuePush("Javascript");
+myQueue.queuePush("is just so");
+myQueue.queuePush("cool");
+myQueue.queuePush(15);
+// unshift-in pop-out
+
+console.log("---YOUR FIRST DATA ---");
+console.log(myQueue.first());
+console.log("---YOUR LAST DATA ---");
+console.log(myQueue.last());
+
+console.log("---YOUR DATA IS ---");
+
+console.log(myQueue.queuePeep());
+
+console.log("---- POP TIME ----");
+console.log(myQueue.queuePop())
+console.log(myQueue.queuePop())
+console.log(myQueue.queuePop())
+console.log(myQueue.queuePop())
+
+console.log(`The Queue is empty ? ${myQueue.isEmpty()}`);
+
+console.log(myQueue.queuePop());
+
+module.exports = Queue
--- stack.js
+++ stack.js
@@ -2,32 +2,93 @@
 
 // Your code implementation
 class Stack {
-  constructor() {
+  constructor(max) {
+    this.arr= [];
+    this.max=max;
   }
 
   size() {
+    return this.arr.length;
   }
 
   stackPush(x) {
+    this.arr.push(x);
   }
 
   stackPop() {
+    if(this.isEmpty()) {
+      return 'You can get in, its empty!';
+    } else {
+      this.arr.pop();
+      return this.stackPeep();
+    }
   }
 
   first() {
+    return this.arr[this.arr.length-1];
   }
 
   last() {
+    return this.arr[0];
   }
 
   isEmpty() {
+    if (this.size() === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isFull() {
+    if (this.size() == this.max ) {
+      return 'Its FULL';
+    } else {
+      return `Come In, there's still plenty of room`;
+    }
   }
 
   stackPeep() {
+    return this.arr
   }
 }
+
+//push-in pop-out
+
+
+//buat stack baru
+let myStack = new Stack();
+//Check apakah stack sudah ada isi atau kosong ?
+console.log(`The stack is empty ? ${myStack.isEmpty()}`) // true
+//True jika Stack kosong
+//False jika Stack Memiliki suatu nilai atau lebih
+
+//Tambahkan tiga
+myStack.stackPush("Javascript");
+myStack.stackPush("is just so");
+myStack.stackPush("cool");
+//
+// //Karena Stack menggunakan Array
+// bisa bisa add tipe data apapun seperti string dan interger
+myStack.stackPush(15);
+
+console.log("---YOUR FIRST DATA ---");
+console.log(myStack.first());
+console.log("---YOUR LAST DATA ---");
+console.log(myStack.last());
+
+console.log("---YOUR DATA IS ---");
+
+console.log(myStack.stackPeep());
+
+console.log("---- POP TIME ----");
+console.log(myStack.stackPop())
+console.log(myStack.stackPop())
+console.log(myStack.stackPop())
+console.log(myStack.stackPop())
+
+console.log(`The Stack is empty ? ${myStack.isEmpty()}`);
+
+console.log(myStack.stackPop());
 
 module.exports = Stack
