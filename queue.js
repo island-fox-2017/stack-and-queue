6080181a4aaf74ad2c605395dd8e0941889818d2
name": null <email": null>
2017-06-21 20:53:02 +0700

    queue and stack DONE

--- queue.js
+++ queue.js
@@ -1,7 +1,44 @@
 'use strict'
 
 class Queue {
-  constructor () {
-
+  constructor() {
+    this.queue = [];
+  }
+  
+  queueInsert(x) {
+    this.queue.push(x);
+  }
+  
+  queueRemove() {
+    if (this.queue[0])
+      delete this.queue[0];
+    else
+      console.log("NOTIF: Nothing to be deleted!");
+  }
+  
+  queueFirst() {
+    console.log(this.queue[0]);
+  }
+  
+  queueEmpty() {
+    if (!this.queue[0]) {
+      console.log("NOTIF: Queue is empty!");
+      return true;
+    }
+    else {
+      console.log("NOTIF: Queue is NOT empty!");
+      return false;
+    }
   }
 }
+
+let myQueue = new Queue();
+
+myQueue.queueInsert(7);
+//myQueue.queueRemove();
+myQueue.queueEmpty();
+myQueue.queueFirst();
+
+console.log();
+console.log();
+console.log();
--- stack.js
+++ stack.js
@@ -3,31 +3,77 @@
 // Your code implementation
 class Stack {
   constructor() {
+    this.stack = [];
+    this.MAX_SIZE = 5;
   }
 
   size() {
+    return this.stack.length;
   }
 
   stackPush(x) {
+    if (this.isFull())
+      console.log('NOTIF: The stack is full!');
+    else
+      this.stack.push(x);
   }
 
   stackPop() {
+    if (this.isEmpty()) {
+      return "NOTIF: Nothing to be deleted!";
+    }
+    else { 
+      this.stack.pop();
+      return "NOTIF: The last element has been deleted!";
+    }
   }
 
   first() {
+    return this.stack[0]?this.stack[0]:"NOTIF: The array is Empty!";
   }
 
   last() {
+    return this.stack[this.stack.length - 1]?this.stack[this.stack.length - 1]:"NOTIF: The array is Empty!";
   }
 
   isEmpty() {
+    return this.stack[0]?false:true;
   }
 
   isFull() {
+    return this.stack.length === this.MAX_SIZE? true:false;
   }
 
-  stackPeep() {
+  stackPeek() {
+    console.log(this.stack[this.stack.length - 1]);
   }
 }
+
+// TEST CASE
+let myStack = new Stack(7);
+
+console.log(`The stack is empty? ${myStack.isEmpty()}`) // true
+
+myStack.stackPush("Javascript");
+myStack.stackPush("is just so");
+myStack.stackPush("cool");
+myStack.stackPush(15);
+
+myStack.stackPeek();
+
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+
+console.log(`The stack is empty? ${myStack.isEmpty()}`);
+
+console.log(myStack.stackPop());
+
+console.log(`The stack is full? ${myStack.isFull()}`);
+
+console.log(myStack.size());
+console.log(myStack.first());
+console.log(myStack.last());
 
 module.exports = Stack
