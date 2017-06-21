b7437cde9f5fc921fb41aadbb5acd25f14d61eca
budhip <ryo.bunraku@yahoo.com>
2017-06-21 19:46:29 +0700

    commit tugas stack-dan-queue.js

--- queue.js
+++ queue.js
@@ -2,6 +2,71 @@
 
 class Queue {
   constructor () {
+    this.datas = [];
+  }
 
+  size() {
+    return this.datas.length;
+  }
+
+  queueInsert(x) {
+    this.datas.push(x);
+  }
+
+  queueRemove() {
+    this.datas.shift();
+  }
+
+  first() {
+    return this.datas[0];
+  }
+
+  last() {
+    return this.datas[this.size()-1];
+  }
+
+  isEmpty() {
+    if (this.size() == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isFull() {
+  }
+
+  queuePeek() {
+    console.log(this.datas[0]);
+  }
+
+  getData() {
+    return this;
   }
 }
+
+let myQueue = new Queue();
+//cek apakah sudah ada isi atau kosong?
+console.log(`The queue is empty? ${myQueue.isEmpty()}`);
+//tambahkan tiga
+myQueue.queueInsert('JavaScript');
+myQueue.queueInsert('is just so');
+myQueue.queueInsert('cool');
+/*
+  karna queue menggunakan array,
+  kita bisa add tipe data apapun seperti string dan integer
+*/
+myQueue.queueInsert(15);
+//lihat data paling atas
+myQueue.queuePeek();
+//Lihat, kita menghapus data satu per satu
+myQueue.queueRemove();
+myQueue.queueRemove();
+myQueue.queueRemove();
+myQueue.queueRemove();
+//cek lagi apakah stack-nya beneran kosong?
+console.log(`The queue is empty? ${myQueue.isEmpty()}`);
+//coba hapus stack kosong, apa yang terjadi?
+myQueue.queueRemove();
+//untuk mendapatkan seluruh data
+console.log(myQueue.getData());
--- stack.js
+++ stack.js
@@ -3,31 +3,74 @@
 // Your code implementation
 class Stack {
   constructor() {
+    this.datas = [];
   }
 
   size() {
+    return this.datas.length;
   }
 
   stackPush(x) {
+    this.datas.push(x);
   }
 
   stackPop() {
+    this.datas.pop();
   }
 
   first() {
+    return this.datas[0];
   }
 
   last() {
+    return this.datas[this.size()-1];
   }
 
   isEmpty() {
+    if (this.size() == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isFull() {
   }
 
-  stackPeep() {
+  stackPeek() {
+    console.log(this.datas[this.size()-1]);
+  }
+
+  getData() {
+    return this;
   }
 }
+
+let myStack = new Stack();
+
+//cek apakah sudah ada isi atau kosong?
+console.log(`The stack is empty? ${myStack.isEmpty()}`);
+//tambahkan tiga
+myStack.stackPush('JavaScript');
+myStack.stackPush('is just so');
+myStack.stackPush('cool');
+/*
+  karna stack menggunakan array,
+  kita bisa add tipe data apapun seperti string dan integer
+*/
+myStack.stackPush(15);
+//lihat data paling atas
+myStack.stackPeek();
+//Lihat, kita menghapus data satu per satu
+myStack.stackPop();
+myStack.stackPop();
+myStack.stackPop();
+myStack.stackPop();
+//cek lagi apakah stack-nya beneran kosong?
+console.log(`The stack is empty? ${myStack.isEmpty()}`);
+//coba hapus stack kosong, apa yang terjadi?
+myStack.stackPop();
+//untuk mendapatkan seluruh data
+console.log(myStack.getData());
 
 module.exports = Stack
