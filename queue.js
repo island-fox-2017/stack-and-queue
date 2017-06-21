d9caee4ab53c4563c9ba81305750d76c6350c424
Ahmad Fajar <shin.fajar@gmail.com>
2017-06-21 16:43:55 +0700

    ahmad fajar - stack & queue

--- queue.js
+++ queue.js
@@ -1,7 +1,80 @@
 'use strict'
 
 class Queue {
-  constructor () {
+  constructor(maxSize) {
+    this.maxSize = maxSize;
+    this.arr = [];
+  }
 
+  size() {
+    return this.arr.length;
+  }
+
+  queueInsert(x) {
+    if (this.isFull()) {
+      return console.log('Queue is full');
+    } else {
+      return this.arr.unshift(x);
+    }
+  }
+
+  queueRemove() {
+    if (this.arr.length === 0) {
+      return 'Queue is empty';
+    } else {
+      return this.arr.pop();
+    }
+  }
+
+  first() {
+    return this.arr[this.arr.length - 1];
+  }
+
+  last() {
+    return this.arr[0];
+  }
+
+  isEmpty() {
+    return this.arr.length === 0;
+  }
+
+  isFull() {
+    return this.arr.length === this.maxSize;
+  }
+
+  queuePeep() {
+    return this.last();
+  }
+
+  getNewSize(newSize) {
+    this.maxSize = newSize;
   }
 }
+
+let kyuw = new Queue(3);
+console.log(kyuw.isEmpty());
+console.log(kyuw.size());
+
+kyuw.queueInsert('a');
+kyuw.queueInsert('b');
+kyuw.queueInsert('c');
+console.log(kyuw.arr);
+
+console.log(kyuw.first());
+console.log(kyuw.last());
+console.log(kyuw.queuePeep());
+console.log(kyuw.isEmpty());
+console.log(kyuw.isFull());
+
+kyuw.queueInsert('d');
+kyuw.getNewSize(4);
+
+console.log(kyuw.isFull());
+kyuw.queueInsert('d');
+console.log(kyuw.size());
+console.log(kyuw.isFull());
+console.log(kyuw.arr);
+
+kyuw.queueRemove()
+kyuw.queueInsert('e')
+console.log(kyuw.arr);
--- stack.js
+++ stack.js
@@ -2,32 +2,82 @@
 
 // Your code implementation
 class Stack {
-  constructor() {
+  constructor(maxSize) {
+    this.maxSize = maxSize;
+    this.arr = [];
   }
 
   size() {
+    return this.arr.length;
   }
 
   stackPush(x) {
+    if (this.isFull()) {
+      return console.log('Stack is full');
+    } else {
+      return this.arr.push(x);
+    }
   }
 
   stackPop() {
+    if (this.arr.length === 0) {
+      return 'Stack is empty';
+    } else {
+      return this.arr.pop();
+    }
   }
 
   first() {
+    return this.arr[0];
   }
 
   last() {
+    return this.arr[this.arr.length - 1];
   }
 
   isEmpty() {
+    return this.arr.length === 0;
   }
 
   isFull() {
+    return this.arr.length === this.maxSize;
   }
 
   stackPeep() {
+    return this.last();
+  }
+
+  getNewSize(newSize) {
+    this.maxSize = newSize;
   }
 }
+
+let setak = new Stack(3);
+console.log(setak.isEmpty());
+console.log(setak.size());
+
+setak.stackPush('a');
+setak.stackPush('b');
+setak.stackPush('c');
+console.log(setak.arr);
+
+console.log(setak.first());
+console.log(setak.last());
+console.log(setak.stackPeep());
+console.log(setak.isEmpty());
+console.log(setak.isFull());
+
+setak.stackPush('d');
+setak.getNewSize(4);
+
+console.log(setak.isFull());
+setak.stackPush('d');
+console.log(setak.size());
+console.log(setak.isFull());
+console.log(setak.arr);
+
+setak.stackPop()
+setak.stackPush('e')
+console.log(setak.arr);
 
 module.exports = Stack
