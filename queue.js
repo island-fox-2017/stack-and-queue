53a45a7b4ff37bd015f6737096402b0ffad3ec81
adnin31 <adnin.ris31@gmail.com>
2017-06-21 21:20:25 +0700

    telah mengerjakan stack dan queue

--- queue.js
+++ queue.js
@@ -2,6 +2,47 @@
 
 class Queue {
   constructor () {
-
+    this.queue = []
   }
+  size() {
+    return this.queue.length
+  }
+  isEmpty() {
+    if(this.size()==0){
+      return true
+    }else {
+      return false
+    }
+  }
+  queuePeep() {
+    console.log(this.queue)
+  }
+  queueInsert(input){
+    this.queue.push(input)
+  }
+  queueRemove(){
+    return this.queue.shift()
+  }
+
 }
+
+let myQueue = new Queue();
+console.log(`The Stack is empty? ${myQueue.isEmpty()}`);
+
+myQueue.queueInsert('Javascript');
+myQueue.queueInsert('is just so');
+myQueue.queueInsert('cool');
+
+myQueue.queueInsert(15);
+
+myQueue.queuePeep();
+
+console.log(myQueue.queueRemove())
+console.log(myQueue.queueRemove())
+console.log(myQueue.queueRemove())
+console.log(myQueue.queueRemove())
+
+
+console.log(`The Stack is empty? ${myQueue.isEmpty()}`);
+
+console.log(myQueue.queueRemove())
--- stack.js
+++ stack.js
@@ -3,15 +3,21 @@
 // Your code implementation
 class Stack {
   constructor() {
+    this.stack = []
   }
 
   size() {
+    return this.stack.length
   }
 
+
   stackPush(x) {
+    this.stack.push(x)
   }
 
   stackPop() {
+     return this.stack.pop()
+
   }
 
   first() {
@@ -21,13 +27,39 @@ class Stack {
   }
 
   isEmpty() {
+    if(this.size()==0){
+      return true
+    }else {
+      return false
+    }
   }
 
   isFull() {
   }
 
   stackPeep() {
+    console.log(this.stack[this.stack.length-1])
   }
 }
+let myStack = new Stack();
+console.log(`The Stack is empty? ${myStack.isEmpty()}`);
+
+myStack.stackPush('Javascript');
+myStack.stackPush('is just so');
+myStack.stackPush('cool');
+
+myStack.stackPush(15);
+
+myStack.stackPeep();
+
+console.log(myStack.stackPop())
+console.log(myStack.stackPop())
+console.log(myStack.stackPop())
+console.log(myStack.stackPop())
+
+
+console.log(`The Stack is empty? ${myStack.isEmpty()}`);
+
+console.log(myStack.stackPop())
 
 module.exports = Stack
