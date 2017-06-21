908a316e25d5c95616c1cd5ac39406bf97224365
anggraito laras <anggraita.laras@gmail.com>
2017-06-22 02:34:20 +0700

    push task stack&queue

--- queue.js
+++ queue.js
@@ -1,7 +1,61 @@
 'use strict'
 
 class Queue {
-  constructor () {
+  constructor (num) {
+    this.check = num,
+    this.arrQ = []
+  }
 
+  size() {
+    return this.arrQ.length;
+  }
+
+  queueInsert(value) {
+    if(this.isMax()) { return console.log("Masimal queue 3")}
+    else{return this.arrQ.push(value)};
+  }
+
+  queueRemove() {
+    return this.arrQ.shift()
+  }
+
+  first() {
+    return this.arrQ[0]
+  }
+
+  last() {
+    return this.arrQ[this.arrQ.length - 1]
+  }
+
+  isEmpty() {
+    return this.arrQ.length == 0
+  }
+
+  isMax() {
+    return this.arrQ.length >= this.check
+  }
+
+  queuePeek() {
+    return this.first()
   }
 }
+
+let objQue = new Queue(3);
+console.log(objQue.isEmpty());
+objQue.queueInsert("Abjad");
+objQue.queueInsert("Huruf");
+console.log(objQue.isMax());
+console.log(objQue.size());
+console.log(objQue.arrQ);
+objQue.queueInsert("Tanda Baca");
+objQue.queueInsert("Kode");
+console.log(objQue.check);
+console.log(objQue.arrQ);
+console.log(objQue.first());
+console.log(objQue.last());
+console.log(objQue.queueRemove());
+console.log(objQue.arrQ);
+console.log(objQue.queuePeek());
+
+
+module.exports = Queue
--- stack.js
+++ stack.js
@@ -1,33 +1,72 @@
-'use strict'
+// 'use strict'
 
 // Your code implementation
 class Stack {
-  constructor() {
+  constructor(maxSize) {
+    this.numSize = maxSize,
+    this.arrSize = [];
   }
 
   size() {
+    return this.arrSize.length;
   }
 
   stackPush(x) {
+    if(this.isFull()) {return console.log('Kebak')}
+    else{return this.arrSize.push(x)}
   }
 
   stackPop() {
+    return this.arrSize.pop();
   }
 
   first() {
+    return this.arrSize[0];
   }
 
   last() {
+    return this.arrSize[this.arrSize.length-1];
   }
 
   isEmpty() {
+    return this.arrSize.length == 0
   }
 
   isFull() {
+    return this.arrSize.length >= this.numSize
   }
 
-  stackPeep() {
+  stackPeek() {
+    return this.last()
   }
 }
+
+let objData = new Stack(5);
+
+console.log(objData.isFull());
+console.log(objData.isEmpty());
+// console.log(objData.arrSize);
+objData.stackPush(7);
+objData.stackPush(2);
+objData.stackPush(45);
+objData.stackPush(88);
+console.log(objData.size());
+console.log(objData.arrSize);
+objData.stackPush(62);
+console.log(objData.size());
+objData.stackPush(3);
+console.log(objData.arrSize);
+console.log(objData.isFull());
+console.log(objData.first());
+console.log(objData.last());
+console.log(objData.stackPop());//pop mengeluarkan array input paling akhir/last in first out
+console.log(objData.stackPeek());
+
+
+
+
+// console.log(objData.size());
+
+
 
 module.exports = Stack
