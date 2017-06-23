7f177eac3ec5f9b0603bd662b1d98f63c36682b2
biliman <fickry.bil.iman@gmail.com>
2017-06-24 00:57:28 +0700

    stack and queue done

--- queue.js
+++ queue.js
@@ -1,7 +1,77 @@
 'use strict'
 
 class Queue {
-  constructor () {
-
+  constructor (max_size) {
+    this.max_size = max_size;
+    this.data = [];
+  }
+  
+  size(){
+    return this.data.length;
+  }
+  
+  queueInsert(x){
+    return this.data.push(x);
+  }
+  
+  queueRemove(){
+    return this.data.shift();
+  }
+  
+  first(){
+    return this.data[this.size() - 1];
+  }
+  
+  last(){
+    return this.data[0];
+  }
+  
+  isEmpty(){
+    return this.size() === 0 // ? true : false
+  }
+  
+  isFull(){
+    return this.size() === this.max_size; // ? true : false
+  }
+  
+  queueTop(){
+    return this.first();
+  }
+  
+  queuePrint(){
+    console.log(this.data);
   }
 }
+
+// Instance class
+let myQueue = new Queue();
+
+// cek apakah queue sudah ada isi atau kosong ?
+console.log(`This queue is empty ? : ${myQueue.isEmpty()}`); // true
+
+// tambahkan tiga data
+myQueue.queueInsert('JavaScript');
+myQueue.queueInsert('is just so');
+myQueue.queueInsert('cool bro!');
+
+// karena queue menggunakan tipe data array, kita bisa add tipe data apapun
+// seperti string dan integer(number)
+myQueue.queueInsert(15);
+
+// lihat data paling atas
+myQueue.queueTop(); // 15
+
+// lihat semua data
+myQueue.queuePrint();
+
+// lihat kita menghapus data satu per satu
+console.log(myQueue.queueRemove());
+console.log(myQueue.queueRemove());
+console.log(myQueue.queueRemove());
+console.log(myQueue.queueRemove());
+
+// cek lagi apakah queue nya beneran kosong ?
+console.log(`This Queue is empty ? : ${myQueue.isEmpty()}`); // false
+
+// coba hapus stack kosong, apa yang terjadi ?
+console.log(myQueue.queueRemove());
--- stack.js
+++ stack.js
@@ -2,32 +2,79 @@
 
 // Your code implementation
 class Stack {
-  constructor() {
+  constructor(max_size) {
+    this.max_size = max_size;
+    this.data = [];
   }
 
   size() {
+    return this.data.length;
   }
 
   stackPush(x) {
+    return this.data.push(x);
   }
 
   stackPop() {
+    return this.data.pop();
   }
 
   first() {
+    return this.data[0];
   }
 
   last() {
+    return this.data[this.size() - 1];
   }
 
   isEmpty() {
+    return this.size() === 0 // ? true : false
   }
 
   isFull() {
+    return this.size() === this.max_size; // ? true : false
   }
 
   stackPeep() {
+    return this.first();
+  }
+  
+  stackPrint() {
+    console.log(this.data);
   }
 }
+
+// Instance class
+let myStack = new Stack()
+
+// Cek apakah stack sudah ada isi atau kosong?
+console.log(`The stack is empty ${myStack.isEmpty()}`); // true
+
+// Tambahkan tiga data
+myStack.stackPush('JavaScript');
+myStack.stackPush('is just so');
+myStack.stackPush('cool Bro!');
+
+// karena stack menggunakan tipe data array, kita bisa add tipe data apapun
+// seperti string atau integer(number)
+myStack.stackPush(15);
+
+// lihat data paling atas
+myStack.stackPeep(); // 15
+
+// lihat semua data
+myStack.stackPrint()
+
+// lihat kita menghapus data satu per satu
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+
+// cek lagi apakah stack nya masih ada isi atau sudah kosong?
+console.log(`The Stack is empty ? ${myStack.isEmpty()}`); // false
+
+//coba hapus stack kosong, apa yang terjadi ?
+console.log(myStack.stackPop());
 
 module.exports = Stack
