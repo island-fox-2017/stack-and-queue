7241a5a67298785fbd7960d67338313f253a5d51
rahid <hidayat.85@gmail.com>
2017-06-24 12:41:44 +0700

    stack & queue done

--- queue.js
+++ queue.js
@@ -1,7 +1,88 @@
 'use strict'
 
 class Queue {
-  constructor () {
+  constructor (maksimum) {
+    this.archive = [];
+    this.maximum = maksimum;
 
   }
+  size() {
+    return this.archive.length;
+  }
+
+  queueInsert(x) {
+    return this.archive.push(x);
+  }
+
+  queueRemove() {
+    return this.archive.shift();
+  }
+
+  first() {
+    return this.archive[0]
+  }
+
+  last() {
+    return this.archive[this.archive.length-1];
+  }
+
+  isEmpty() {
+    if (this.archive.length === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  isFull() {
+    return this.maximum === this.archive.length;
+  }
+
+  queuePeek() {
+    return this.archive[this.archive.length-1];
+  }
 }
+
+let myQueue = new Queue (5);
+
+//cek data apakah kosong atau tidak
+console.log(`The queue is empty? ${myQueue.isEmpty()}`); //true
+
+//tambahkan data
+myQueue.queueInsert("JavaScript")
+myQueue.queueInsert("is just so")
+myQueue.queueInsert("cool")
+
+//cek isi data
+console.log(myQueue.archive);
+
+//cek berapa banyak data sekarang
+console.log(`The queue size: ${myQueue.size()}`)
+
+//tambahkan lagi data
+myQueue.queueInsert(15)
+myQueue.queueInsert('Pick me')
+
+//cek data apakah kosong atau tidak
+console.log(`The queue is empty? ${myQueue.isEmpty()}`); //false
+
+//cek isi data
+console.log(myQueue.archive);
+
+//cek data apakh full/tidak
+console.log(`The queue is full? ${myQueue.isFull()}`);
+
+//Lihat data paling atas
+console.log(myQueue.queuePeek());
+
+//Hapus data satu persatu
+console.log(myQueue.queueRemove());
+console.log(myQueue.queueRemove());
+console.log(myQueue.queueRemove());
+console.log(myQueue.queueRemove());
+console.log(myQueue.queueRemove());
+
+console.log(`The queue is empty? ${myQueue.isEmpty()}`);
+
+console.log(myQueue.queueRemove()); //undefined
--- stack.js
+++ stack.js
@@ -2,32 +2,90 @@
 
 // Your code implementation
 class Stack {
-  constructor() {
+  constructor(maksimum) {
+    this.archive = [];
+    this.maximum = maksimum;
   }
 
   size() {
+    return this.archive.length;
   }
 
   stackPush(x) {
+    return this.archive.push(x);
   }
 
   stackPop() {
+    return this.archive.pop();
   }
 
   first() {
+    return this.archive[0]
   }
 
   last() {
+    return this.archive[this.archive.length-1];
   }
 
   isEmpty() {
+    if (this.archive.length === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   isFull() {
+    return this.maximum === this.archive.length;
   }
 
-  stackPeep() {
+  stackPeek() {
+    return this.archive[this.archive.length-1];
   }
 }
+
+let myStack = new Stack(5)
+
+//cek data apakah kosong atau tidak
+console.log(`The stack is empty? ${myStack.isEmpty()}`); //true
+
+//tambahkan data
+myStack.stackPush("JavaScript")
+myStack.stackPush("is just so")
+myStack.stackPush("cool")
+
+//cek isi data
+console.log(myStack.archive);
+
+//cek berapa banyak data sekarang
+console.log(`The stack size: ${myStack.size()}`)
+
+//tambahkan lagi data
+myStack.stackPush(15)
+myStack.stackPush('Pick me')
+
+//cek data apakah kosong atau tidak
+console.log(`The stack is empty? ${myStack.isEmpty()}`); //false
+
+//cek isi data
+console.log(myStack.archive);
+
+//cek data apakah full/tidak
+console.log(`The stack is full? ${myStack.isFull()}`);
+
+//Lihat data paling atas
+console.log(myStack.stackPeek());
+
+//Hapus data satu persatu
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+console.log(myStack.stackPop());
+
+console.log(`The stack is empty? ${myStack.isEmpty()}`);
+
+console.log(myStack.stackPop()); //undefined
 
 module.exports = Stack
