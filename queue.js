3bc7a38ee5f28f2685980f6f4b47ce59896d1f5f
hariantara <hariantara.iputu@gmail.com>
2017-07-02 18:29:18 +0700

    stack and queue

--- queue.js
+++ queue.js
@@ -1,7 +1,60 @@
 'use strict'
 
-class Queue {
-  constructor () {
+class Queue
+{
+  constructor ()
+  {
+    this.arr = []
+
 
   }
+
+    size()
+    {
+      return this.arr.length = 5;
+    }
+
+    queuepush(x)
+    {
+      this.arr.push(x)
+      return "Push Berhasil";
+    }
+
+    queuePop()
+    {
+      return this.arr.pop()
+    }
+
+    isEmpty()
+    {
+      if(this.arr.length == 0)
+      {
+        return "Queue still empty";
+      }
+      else
+      {
+        return "Queue is filled";
+      }
+
+    isFull()
+    {
+      if(this.size() == 5)
+      {
+        return "Sorry queue is full";
+      }
+      else
+      {
+        return "The queue can still be filled again"
+      }
+    }
+
+    queuePeep()
+    {
+      return this.arr[0];
+    }
+  }
 }
+
+let myQueue = new Queue();
+
+console.log(`The stack is empty? ${myQueue.isEmpty()}`);
--- stack.js
+++ stack.js
@@ -3,31 +3,83 @@
 // Your code implementation
 class Stack {
   constructor() {
+    this.arr = [];
+
+    // this.long = this.arr.length
   }
 
   size() {
+    return this.arr.length = 3;
   }
 
   stackPush(x) {
+    this.arr.push(x);
+    return "Push berhasil";
   }
 
   stackPop() {
+    return this.arr.pop();
   }
 
   first() {
+    return this.arr[0];
   }
 
   last() {
+    return this.arr.slice(-1).pop
   }
 
   isEmpty() {
+    if(this.arr.length == 0)
+    {
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 
   isFull() {
+    if(this.size() == 3 )
+    {
+      return "Sorry, stack is Full";
+    }
+    else {
+      return "The stack can still be filled again";
+    }
   }
 
   stackPeep() {
+    return this.last();
   }
 }
+
+let myStack = new Stack();
+
+console.log(`The stack is empty? ${myStack.isEmpty()}`);
+
+myStack.stackPush("JavaScript");
+myStack.stackPush("is just so");
+myStack.stackPush("cool");
+
+myStack.stackPush(15);
+myStack.stackPush("last");
+
+myStack.stackPeep();
+
+console.log(myStack.isFull());
+
+console.log(myStack);
+// console.log(myStack.stackPush("Java Script"));
+//
+// console.log(myStack.stackPush("ULER"));
+//
+// console.log(myStack.size());
+//
+// console.log(myStack.stackPop());
+
+// myStack.stackPop();
+
+
 
 module.exports = Stack
