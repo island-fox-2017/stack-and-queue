1ed8120e889fd037c0b39bff4773727b6830fe29
Adith <adithwidyapradipta@gmail.com>
2017-07-02 21:58:01 +0700

    stack and queue done

--- queue.js
+++ queue.js
@@ -1,7 +1,83 @@
 'use strict'
 
 class Queue {
-  constructor () {
-
+  constructor(maximum) {
+    this.max = maximum;
+    this.queueLine = [];
   }
-}
+
+  size() {
+    return 'Current people in line is: ' + this.queueLine.length;
+  }
+
+  queueInsert(x) {
+    if (this.isFull()){
+      return 'Sorry, there are no space left.';
+    } else {
+      return this.queueLine.push(x);
+    }
+  }
+
+  queueComeInside(){
+    if (this.isEmpty()){
+      return 'The line is empty.';
+    } else {
+      return this.queueLine.shift();
+    }
+  }
+
+  first() {
+    return `First in queue: ${this.queueLine[0]}`;
+  }
+
+  last() {
+    let lastIndex = this.queueLine.length - 1;
+    return `Last in queue: ${this.queueLine[lastIndex]}`;
+  }
+
+  isEmpty() {
+    return this.size === 0; //kosong saat length === 0
+  }
+
+  isFull() {
+    return this.queueLine.length >= this.max; //penuh saat length === value dari parameter max
+  }
+
+  stackPeep() {
+    return this.first();
+  }
+}//class
+
+//INSTANCE
+var iPhone8Line = new Queue (5); //There is 5 guys already in line at 2 AM
+
+//Private testcase
+//Data push
+iPhone8Line.queueInsert('1. John');
+iPhone8Line.queueInsert('2. Marquees');
+iPhone8Line.queueInsert('3. Justine');
+iPhone8Line.queueInsert('4. Lamarr');
+iPhone8Line.queueInsert('5. Austin');
+
+//Data check
+console.log('  ');
+console.log('Max line this morning is 5');
+console.log('=====QUEUE CHECK=====');
+console.log('-----Queue-----');
+console.log(iPhone8Line.queueLine);
+console.log('-----First Person-----');
+console.log(iPhone8Line.first());
+console.log('-----Last Person-----');
+console.log(iPhone8Line.last());
+console.log(iPhone8Line.size());
+
+
+//Data manipulations
+console.log(' ');
+console.log('=====DATA MANIPULATIONS=====');
+console.log('-----Someone\'s trying to get in line-----');
+console.log(iPhone8Line.queueInsert('6. Lou'));
+console.log('-----First person may come inside first-----');
+console.log(iPhone8Line.queueComeInside());
+console.log('-----Last person to come inside-----');
+console.log(iPhone8Line.last());
--- stack.js
+++ stack.js
@@ -2,32 +2,83 @@
 
 // Your code implementation
 class Stack {
-  constructor() {
+  constructor(maximum) {
+    this.max = maximum;
+    this.arrStack = [];
   }
 
   size() {
+    return 'Your current stack is: ' + this.arrStack.length;
   }
 
   stackPush(x) {
+    if (this.isFull()) {
+      return 'Sorry, data is already full.';
+    } else {
+      return this.arrStack.push(x);
+    }
   }
 
   stackPop() {
+    if (this.isEmpty()) {
+      return 'Sorry, you can\'t delete anything from void'
+    } else {
+      return this.arrStack.pop();
+    }
   }
 
   first() {
+    return `Your first stack: ${this.arrStack[0]}`;
   }
 
   last() {
+    let lastIndex = this.arrStack.length-1;
+    return `Your last stack: ${this.arrStack[lastIndex]}`;
   }
 
   isEmpty() {
+    return this.size === 0; //kosong saat length === 0
   }
 
   isFull() {
+    return this.arrStack.length >= this.max; //penuh saat length === value dari parameter max
   }
 
   stackPeep() {
+    return this.first();
   }
 }
+
+//INSTANCE
+var deckOfCards = new Stack (8); //We try to stack 8 cards
+
+//Private testcase
+//Data push
+deckOfCards.stackPush('2 Hearts');
+deckOfCards.stackPush('King Hearts');
+deckOfCards.stackPush('10 Clover');
+deckOfCards.stackPush('4 Diamonds');
+deckOfCards.stackPush('Jack Pikes');
+deckOfCards.stackPush('Queen Diamonds');
+deckOfCards.stackPush('Joker');
+deckOfCards.stackPush('2 Pikes');
+
+//Data check
+console.log('=====DATA CHECK=====');
+console.log('-----Your Stack-----');
+console.log(deckOfCards.arrStack);
+console.log('-----First Stack-----');
+console.log(deckOfCards.first());
+console.log('-----Last Stack-----');
+console.log(deckOfCards.last());
+console.log(deckOfCards.size());
+
+
+//Data manipulations
+console.log(' ');
+console.log('=====DATA MANIPULATIONS=====');
+console.log(deckOfCards.stackPush('3 Hearts'));
+
+
 
 module.exports = Stack
