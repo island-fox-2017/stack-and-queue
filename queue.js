db71a3f5df254ff2be4a179db017969a08a7443a
achimbaggins <achim_baggins@yahoo.com>
2017-07-03 07:51:58 +0700

    Tugas Stack dan Queue

--- queue.js
+++ queue.js
@@ -1,7 +1,55 @@
 'use strict'
 
 class Queue {
-  constructor () {
+  constructor(kapasitas) {
+    this.kapasitas = kapasitas
+    this.queueArr = []
+  }
 
+  size() {
+    return this.queueArr.length;
+  }
+
+  queuePush(x) {
+    if(this.queueArr.length == this.kapasitas){return "daya tampung sudah maximal";}
+    else {
+      return this.queueArr.push(x);
+    }
+  }
+
+  queueRemove() {
+    return this.queueArr.shift();
+  }
+
+  first() {
+    return this.queueArr[0];
+  }
+
+  last() {
+    return this.queueArr[this.queueArr.length-1];
+  }
+
+  isEmpty() {
+    return this.queueArr.length === 0 ? true : false;
+  }
+
+  isFull() {
+    return this.queueArr.length == this.kapasitas;
+  }
+
+  queuePeep() {
+    // return this.stackArr[this.stackArr.length-1]
+    return last();
   }
 }
+
+let queue1 = new Queue(5);
+console.log(`Antrian ini kosong? ${queue1.isEmpty()}`);
+queue1.queuePush('Masakan');
+console.log(queue1);
+console.log(`Antrian ini kosong? ${queue1.isEmpty()}`);
+queue1.queuePush('Minuman');
+queue1.queuePush('Snack');
+console.log(queue1);
+
+console.log(`Yang pertama masuk : ${queue1.first()}`);
--- stack.js
+++ stack.js
@@ -2,32 +2,47 @@
 
 // Your code implementation
 class Stack {
-  constructor() {
+  constructor(kapasitas) {
+    this.kapasitas = kapasitas
+    this.stackArr = []
   }
 
   size() {
+    return this.stackArr.length;
   }
 
   stackPush(x) {
+    if(isFull){return "daya tampung sudah maximal";}
+    else {return this.stackArr.push(x);}
   }
 
   stackPop() {
+    return this.stackArr.pop();
   }
 
   first() {
+    return this.stackArr[0];
   }
 
   last() {
+    return this.stackArr[this.stackArr.length-1];
   }
 
   isEmpty() {
+    return this.stackArr.length === 0 ? true : false;
   }
 
   isFull() {
+    return this.stackArr.length == this.kapasitas;
   }
 
   stackPeep() {
+    // return this.stackArr[this.stackArr.length-1]
+    return last();
   }
 }
 
+var input1 = new Stack(5);
+console.log(input1.stackPush(5));
+console.log(input1.size());
 module.exports = Stack
