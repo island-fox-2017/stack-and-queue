d3fe91ff5102e94cb19f0bb1fcba01dc2eeb4ad9
gananggww <gananggww@gmail.com>
2017-07-03 09:15:30 +0700

    stack and queue

--- queue.js
+++ queue.js
@@ -1,7 +1,49 @@
 'use strict'
 
 class Queue {
-  constructor () {
+  constructor (input) {
+    this.input = input
+    this.range = []
 
   }
+  size() { //ngukur panjang inputan
+  return this.range.length;
+  }
+  stackPush(namaLengkap) { //data inputan
+  return this.range.push(namaLengkap);
+  }
+  stackPop() {//ngambil data inputan dari yg index.length-1
+  return this.range.pop()
+  }
+  first() { //nampilin index pertama/0
+    return this.range[0];
+  }
+  last() { //nampilin index terahir
+    return this.range[this.range.length-1]
+  }
+
+  isEmpty() { //indikator kalo kosong =true
+    return this.range == 0
+  }
+
+  isFull() {//indikator kalo kosong = true
+    return this.range.length == this.input
+  }
+
+  stackPeep() {
+    return this.last ();
+  }
 }
+
+var ukur = new Queue(2);
+console.log(ukur.size());
+ukur.stackPush("Ganang")
+ukur.stackPush("Wahyu")
+ukur.stackPush("Wicaksono")
+console.log(ukur.stackPop());
+console.log(ukur.first());
+console.log(ukur.range);
+console.log(ukur.last())
+console.log(ukur.isEmpty());
+console.log(ukur.isFull());
+console.log(ukur.stackPeep());
--- stack.js
+++ stack.js
@@ -2,32 +2,48 @@
 
 // Your code implementation
 class Stack {
-  constructor() {
+  constructor(input) {
+    this.input = input;
+    this.range = []
+  }
+  size() { //ngukur panjang inputan
+    return this.range.length;
+  }
+  stackPush(namaLengkap) { //data inputan
+    return this.range.push(namaLengkap);
+  }
+  stackPop() {//ngambil data inputan dari yg index.length-1
+   return this.range.pop()
+  }
+  first() { //nampilin index pertama/0
+    return this.range[0];
+  }
+  last() { //nampilin index terahir
+    return this.range[this.range.length-1]
   }
 
-  size() {
+  isEmpty() { //indikator kalo kosong =true
+    return this.range == 0
   }
 
-  stackPush(x) {
-  }
-
-  stackPop() {
-  }
-
-  first() {
-  }
-
-  last() {
-  }
-
-  isEmpty() {
-  }
-
-  isFull() {
+  isFull() {//indikator kalo kosong = true
+    return this.range.length == this.input
   }
 
   stackPeep() {
+    return this.first ();
   }
 }
-
+var ukur = new Stack (2)
+console.log(ukur.size());
+ukur.stackPush("Ganang")
+ukur.stackPush("Wahyu")
+ukur.stackPush("Wicaksono")
+console.log(ukur.stackPop());
+console.log(ukur.first());
+console.log(ukur.range);
+console.log(ukur.last())
+console.log(ukur.isEmpty());
+console.log(ukur.isFull());
+console.log(ukur.stackPeep());
 module.exports = Stack
