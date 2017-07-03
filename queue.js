f9237cd5846d7e60e236be155eb870a42e0ccc48
anto <irianto223@gmail.com>
2017-07-03 18:04:23 +0700

    sudah diperiksa

--- queue.js
+++ queue.js
@@ -1,7 +1,43 @@
 'use strict'
+const MAX = 3
 
 class Queue {
   constructor () {
+    this.data = []
+  }
 
+  queueLihat() {
+    return this.data
+  }
+
+  queuePush(input) {
+    if (!this.isFull()) {
+      this.data.push(input)
+    }
+    else {
+      console.log('array sudah penuh!');
+    }
+  }
+
+  queueShift() {
+    this.data.shift()
+  }
+
+  isFull() {
+    if (this.data.length == MAX) {
+      return true
+    }
+    else {
+      return false
+    }
   }
 }
+
+let queue1 = new Queue()
+queue1.queuePush(2)
+queue1.queuePush(4)
+queue1.queuePush(6)
+queue1.queuePush(8)
+console.log(queue1.queueLihat())
+queue1.queueShift()
+console.log(queue1.queueLihat())
--- stack.js
+++ stack.js
@@ -1,33 +1,71 @@
 'use strict'
+const MAX = 3
 
 // Your code implementation
 class Stack {
   constructor() {
+    this.data = []
   }
 
   size() {
+    return this.data.length
   }
 
   stackPush(x) {
+    if (!this.isFull()) {
+      this.data.push(x)
+    }
+    else {
+      console.log('array sudah full!')
+    }
   }
 
   stackPop() {
+    this.data.pop()
   }
 
   first() {
+    return this.data[0]
   }
 
   last() {
+    return this.data[this.data.length-1]
   }
 
   isEmpty() {
+    if (this.data[0] != '') {
+      return false
+    }
+    else {
+      return true
+    }
   }
 
   isFull() {
+    if (this.data.length == MAX) {
+      return true
+    }
+    else {
+      return false
+    }
   }
 
   stackPeep() {
+    return this.data
   }
 }
+
+
+let stack1 = new Stack()
+stack1.stackPush(1)
+stack1.stackPush(3)
+stack1.stackPush(5)
+stack1.stackPush(7)
+console.log(stack1.stackPeep())
+console.log(stack1.isFull())
+console.log(stack1.isEmpty())
+console.log(stack1.size())
+console.log(stack1.first())
+console.log(stack1.last())
 
 module.exports = Stack
